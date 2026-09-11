254b0a1fe93563452a86fcbe1afa607ad1f9a28d
Ignacio Hernández <ih43108@gmail.com>
2026-09-10 21:23:36 -0500

    feat(videojuegos): resuelve conflicto y guarda rutas

---
+++ backend/src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+// Rutas individuales
+router.use('/jugadores', require('./jugadores.routes'));
+
+ router.use('/videojuegos', require('./videojuegos.routes'));
+// router.use('/puntuaciones', require('./puntuaciones.routes'));
+// router.use('/', require('./dashboard.routes'));
+
+module.exports = router;
